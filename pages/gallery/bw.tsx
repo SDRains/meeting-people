7a5913e0d8d402694b4b4f1ac7a4969e846d8062
Stephen Rains <applerains1234@gmail.com>
2023-02-20 10:23:20 -0700

    Optimizing image loading times

--- pages/gallery/bw.tsx
+++ pages/gallery/bw.tsx
@@ -19,51 +19,52 @@ export default function gallery({fileNames}) {
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 sm:gap-6">
                             <>
                                 {fileNames.map((imageName: string) => (
-                                    <>
+                                    <div key={imageName}>
                                         <div className="flex items-center hover:opacity-90 hover:cursor-pointer" key={imageName} onClick={() => {
                                             setShowPopup(true)
                                             setSelectedImage(imageName)
                                         }}>
+
                                             <img src={`/images/bw/${imageName}.jpg`} className="w-full" />
                                         </div>
+                                    </div>
+                                ))}
 
-                                        <Transition.Root show={showPopup} as={Fragment}>
-                                            <Dialog as="div" className="relative z-10" onClose={setShowPopup}>
+                                <Transition.Root show={showPopup} as={Fragment}>
+                                    <Dialog as="div" className="relative z-10" onClose={setShowPopup}>
+                                        <Transition.Child
+                                            as={Fragment}
+                                            enter="ease-out duration-300"
+                                            enterFrom="opacity-0"
+                                            enterTo="opacity-100"
+                                            leave="ease-in duration-200"
+                                            leaveFrom="opacity-100"
+                                            leaveTo="opacity-0"
+                                        >
+                                            <div className="fixed inset-0 bg-gray-200 bg-opacity-20 transition-opacity" />
+                                        </Transition.Child>
+
+                                        <div className="fixed inset-0 z-10 overflow-y-auto">
+                                            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                                                 <Transition.Child
                                                     as={Fragment}
                                                     enter="ease-out duration-300"
-                                                    enterFrom="opacity-0"
-                                                    enterTo="opacity-100"
+                                                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                                    enterTo="opacity-100 translate-y-0 sm:scale-100"
                                                     leave="ease-in duration-200"
-                                                    leaveFrom="opacity-100"
-                                                    leaveTo="opacity-0"
+                                                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                                 >
-                                                    <div className="fixed inset-0 bg-gray-200 bg-opacity-20 transition-opacity" />
+                                                    <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-6">
+                                                        <div>
+                                                            <img src={`/images/bw/${selectedImage}.jpg`} className="w-full pt-2" />
+                                                        </div>
+                                                    </Dialog.Panel>
                                                 </Transition.Child>
-
-                                                <div className="fixed inset-0 z-10 overflow-y-auto">
-                                                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                                                        <Transition.Child
-                                                            as={Fragment}
-                                                            enter="ease-out duration-300"
-                                                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                                                            leave="ease-in duration-200"
-                                                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                                                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                                        >
-                                                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-6">
-                                                                <div>
-                                                                    <img src={`/images/bw/${selectedImage}.jpg`} className="w-full pt-2" />
-                                                                </div>
-                                                            </Dialog.Panel>
-                                                        </Transition.Child>
-                                                    </div>
-                                                </div>
-                                            </Dialog>
-                                        </Transition.Root>
-                                    </>
-                                ))}
+                                            </div>
+                                        </div>
+                                    </Dialog>
+                                </Transition.Root>
                             </>
                         </div>
                     </div>
